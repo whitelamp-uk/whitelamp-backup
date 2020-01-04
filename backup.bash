9f22db3cf24d2ff77d3cc14f491bb1ede91c02b3
Mark Page <mark.page@whitelamp.com>
2020-01-04 19:53:42 +0000

    Adds desktop notification

--- backup.bash
+++ backup.bash
@@ -1,12 +1,11 @@
 
-
-prog="/usr/bin/rsync"
+rsn="/usr/bin/rsync"
 tmp="$(dirname $0)/backup.tmp"
 tmp2="$(dirname $0)/backup.tmp2"
 
 if [ -f "$tmp" ]
 then
-    echo "Temporary file $tmp already exists" > /dev/stderr
+    echo "Temporary file $tmp already exists" >&2
     exit 101
 fi
 
@@ -31,17 +30,17 @@ do
     # Check config and make destination directory if necessary
     if [ ! "$t" ]
     then
-        echo "Trash file not defined" > /dev/stderr
+        echo "Trash file not defined" >&2
         continue
     fi
     if [ ! -d "$s" ]
     then
-        echo "Source \"$s\" is not a directory" > /dev/stderr
+        echo "Source \"$s\" is not found"
         continue
     fi
     if [ ! -d "$(dirname "$d")" ]
     then
-        echo "Destination parent directory \"$(dirname "$d")\" not found" > /dev/stderr
+        echo "Destination parent directory \"$(dirname "$d")\" not found" >&2
         continue
     fi
     if [ ! -d "$d" ]
@@ -49,13 +48,13 @@ do
         mkdir "$d"
         if [ $? != 0 ]
         then
-            echo "Destination directory \"$d\" could not be created" > /dev/stderr
+            echo "Destination directory \"$d\" could not be created" >&2
             continue
         fi
     fi
 
     # Log source files deleted (destination files are always kept)
-    $prog -rv $o --delete --dry-run "$s/" "$d/" | grep '^deleting\s' | cut -d' ' -f2- > "$tmp"
+    $rsn -rv $o --delete --dry-run "$s/" "$d/" | grep '^deleting\s' | cut -d' ' -f2- > "$tmp"
     touch "$s/$t"
     echo -n "" > "$tmp2"
     ts="$(date '+%FT%T')"
@@ -76,8 +75,9 @@ do
     cat "$tmp2" >> "$s/$t"
 
     # rsync
-    echo $ts $prog -r $o "$s/" "$d/"
-    $prog -r $o "$s/" "$d/"
+    echo $ts $rsn -r $o "$s/" "$d/"
+    $rsn -r $o "$s/" "$d/"
+    XDG_RUNTIME_DIR=$1 notify-send --urgency=low --category=transfer.complete --expire-time=4000 "Backed up" "$rsn -r $o $s/ $d/"
 
 done < "$(dirname $0)/backup.cfg"
 
